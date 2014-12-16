dfbdae0030ad884b82bc4de370193f6b94ee8548
Geoffrey Roberts <groberts@anchor.net.au>
2014-12-16 18:10:00 +1100

    Added test that fails hlint

---
+++ hlint-noopt/Main.hs
@@ -0,0 +1,20 @@
+module Main where
+
+import Data.List
+
+main :: IO ()
+main = do
+    doSomething
+
+doSomething :: IO ()
+doSomething = do
+    let f = Foop "hi" "there" 123
+    let g = Foop "hi" "there"
+    putStrLn $ show f
+    putStrLn (show (g 124))
+
+data Foop = Foop {
+    foopFoo      :: [Char],
+    foopBar      :: [Char],
+    foopGlarChar :: Int
+} deriving (Show)
